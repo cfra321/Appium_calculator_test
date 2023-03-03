46f455981d3fda8bc02fd8ebb436e9a3c4070431
cfra321 <kukuhwicaksono105xb@gmail.com>
2023-03-03 18:16:48 +0700

    appium calculator

---
+++ src/main/java/com/juaracoding/appium/page/Calculator.java
@@ -0,0 +1,47 @@
+package com.juaracoding.appium.page;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.support.PageFactory;
+
+
+public class Calculator {
+
+    public AndroidDriver<MobileElement> driver;
+
+    public Calculator(AndroidDriver<MobileElement> driver) {
+        this.driver = driver;
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+    }
+
+    // Locator number
+    @AndroidFindBy(xpath = "//android.widget.ImageButton[@content-desc=\"1\"]")
+    private MobileElement btnSatu;
+    @AndroidFindBy(id = "com.google.android.calculator:id/digit_2")
+    private MobileElement btnDua;
+
+
+    // locator aritmatika
+    @AndroidFindBy(xpath = "//android.widget.ImageButton[@content-desc=\"plus\"]")
+    private MobileElement btnTambah;
+
+    @AndroidFindBy(id = "com.google.android.calculator:id/eq")
+    private MobileElement btnSamaDengan;
+
+    // locator result
+    @AndroidFindBy(id = "com.google.android.calculator:id/result_final")
+    private MobileElement result;
+
+    public void calcAdd() {
+        btnSatu.click();
+        btnTambah.click();
+        btnDua.click();
+        btnSamaDengan.click();
+    }
+    public String getTxtResult() {
+        return result.getText();
+    }
+
+}
---
+++ src/test/java/com/juaracoding/appium/TestCalculator.java
@@ -0,0 +1,45 @@
+package com.juaracoding.appium;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import com.juaracoding.appium.page.Calculator;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+public class TestCalculator {
+
+    private static AndroidDriver<MobileElement> driver;
+    private Calculator calculator;
+
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("deviceName", "Pixel_2_API_24");
+        capabilities.setCapability("uuid", "emulator-5554");
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("platformVersion", "7.0");
+        capabilities.setCapability("appPackage", "com.google.android.calculator");
+        capabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
+        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+    @BeforeMethod
+    public void pageObject(){
+        calculator=new Calculator(driver);
+    }
+    @Test
+    public void testAdd(){
+        calculator.calcAdd();
+        System.out.println("Hasil = "+calculator.getTxtResult());
+        Assert.assertEquals(calculator.getTxtResult(),"3");
+    }
+
+    @AfterClass
+    public void closeApp() {
+        driver.quit();
+    }
+}
